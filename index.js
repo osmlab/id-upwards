2549e67ac0c672d882d8952a6494c45ef603dc0a
Tom MacWright <tom@macwright.org>
2016-10-13 19:12:17 -0400

    Make boring

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const mapboxgl = require('mapbox-gl');
-const choo = require('choo')
-const html = require('choo/html')
+const React = require('react')
+const ReactDOM = require('react-dom')
 
 mapboxgl.accessToken = 'pk.eyJ1IjoidG1jdyIsImEiOiJIZmRUQjRBIn0.lRARalfaGHnPdRcc-7QZYQ';
 
@@ -19,127 +19,125 @@ var empty = {
     },
   "layers": [
     {
-      "id": "osm",
-      "source": "osm",
-      "type": "line",
-      "paint": {
-      }
-    },
-    {
       "id": "background",
       "type": "background",
       "paint": {
-        "background-color": "rgba(0,0,0,0)"
+        "background-color": "#fff"
+      }
+    },
+    {
+      "id": "osm-circle",
+      "source": "osm",
+      "type": "circle",
+      "paint": {
+          'circle-color': '#000',
+          'circle-radius': 2
+      }
+    },
+    {
+      "id": "osm-line",
+      "source": "osm",
+      "type": "line",
+      "paint": {
+          'line-color': '#000',
+          'line-width': 2
+      }
+    },
+    {
+      "id": "osm-fill",
+      "source": "osm",
+      "type": "fill",
+      "paint": {
+          'fill-color': '#f0f'
       }
     }
   ]
 };
 // var all = context.intersects(map.extent()) 
 
-function startApp(context, node) {
-    const app = choo();
 
-    function featuresToGeoJSON(entities) {
-        var features = [];
-        for (var id in entities) {
-            if (id[0] === 'n' && context.graph().parentWays(entities[id]).length) continue;
-            try {
-                var gj = entities[id].asGeoJSON(context.graph());
-                if (gj.type === 'FeatureCollection') continue;
-                features.push({
-                    type: 'Feature',
-                    properties: {},
-                    geometry: gj
-                });
-            } catch(e) {
-                console.error(e);
+    var App = React.createClass({
+        featuresToGeoJSON(entities) {
+            var context = this.props.context;
+            var features = [];
+            for (var id in entities) {
+                if (id[0] === 'n' && context.graph().parentWays(entities[id]).length) continue;
+                try {
+                    var gj = entities[id].asGeoJSON(context.graph());
+                    if (gj.type === 'FeatureCollection') continue;
+                    features.push({
+                        type: 'Feature',
+                        properties: {},
+                        geometry: gj
+                    });
+                } catch(e) {
+                    console.error(e);
+                }
             }
+            return {
+                type: 'FeatureCollection',
+                features: features
+            };
+        },
+        getInitialState() {
+            return  {
+                active: false,
+                map: undefined,
+                features: []
+            };
+        },
+        componentDidMount: function() {
+            this.props.context.on('change.idupwards', (e) => {
+                this.setState({ geojson: this.featuresToGeoJSON(this.props.context.graph().base().entities) });
+            });
+            this.props.context.history().on('change.idupwards', (e) => {
+                this.setState({ geojson: this.featuresToGeoJSON(this.props.context.graph().base().entities) });
+            });
+        },
+        componentDidUpdate() {
+            var state = this.state;
+            if (this.state.active && this.state.geojson && this.mapElement) {
+                if (this.map && this.map.getSource('osm')) {
+                    this.map.getSource('osm').setData(this.state.geojson);
+                } else {
+                    this.map = new mapboxgl.Map({
+                        container: this.mapElement,
+                        style: empty,
+                    });
+                    this.map.fitBounds([
+                        this.props.context.map().extent()[0],
+                        this.props.context.map().extent()[1]
+                    ]);
+                    console.log(this.state.geojson);
+                    this.map.on('load', () => {
+                        this.map.getSource('osm').setData(this.state.geojson);
+                    });
+                }
+            }
+        },
+        toggle: function() {
+            this.setState({
+                active: !this.state.active
+            });
+        },
+        render: function () {
+            var state = this.state;
+            return <div className='map-control'>
+                <button tabIndex='-1' style={{color:'white'}} onClick={this.toggle}>
+                    3D
+                </button>
+                {state.active ?
+                    <div className='help-wrap map-overlay fillL col5 content'>
+                        <div style={{height:500}} ref={elem => { this.mapElement = elem }}></div>
+                    </div> : null}
+                </div>;
         }
-        return {
-            type: 'FeatureCollection',
-            features: features
-        };
-    }
-
-    app.model({
-      state: {
-          active: false,
-          map: undefined,
-          features: []
-      },
-      subscriptions: [
-          (send, done) => {
-              context.on('change.idupwards', function(e) {
-                  send('geojson', {
-                      geojson: featuresToGeoJSON(context.graph().base().entities)
-                  }, () => {
-                      done()
-                  });
-              });
-          },
-          (send, done) => {
-              context.history().on('change.idupwards', function(e) {
-                  send('geojson', { geojson: featuresToGeoJSON(context.graph().base().entities) }, () => {
-                      done()
-                  });
-              });
-          }
-      ],
-      reducers: {
-        toggle: (data, state) => ({ active: !state.active }),
-        geojson: (data, state) => ({ geojson: data.geojson }),
-        map: (data, state) => ({ map: data.map })
-      }
     });
-
-    const mainView = (state, prev, send) => {
-
-        function drawMap(elem) {
-            setTimeout(function() {
-        if (state.active) {
-        var map;
-        if (state.map) {
-          map = state.map;
-              map.getSource('osm').setData(state.geojson);
-        } else {
-          map = new mapboxgl.Map({
-              container: 'id-upwards-map',
-              style: empty
-          });
-          map.on('load', function() {
-              map.getSource('osm').setData(state.geojson);
-          });
-          send('map', { map: map });
-        }
-        }
-            }, 10);
-        }
-
-        return html`
-          <div class='map-control'>
-            <button tabindex='-1' style='color:white' onclick=${(e) => send('toggle')}>
-              3D
-            </button>
-            ${state.active ? html`
-            <div class='help-wrap map-overlay fillL col5 content'>
-              <div onload=${drawMap} style='height:500px;' id='id-upwards-map'></div>
-            </div>` : ``}
-          </div>
-        `;
-    };
-
-    app.router((route) => [
-      route('/', mainView)
-    ]);
-
-    const tree = app.start();
-    node.appendChild(tree);
-}
 
 id.ui().pluginRegisterControl({
     handler: function(context) {
         return function(selection) {
-            startApp(context, selection.node());
+            ReactDOM.render(<App context={context} />, selection.node());
         };
     },
     buttonClass: 'hi'
